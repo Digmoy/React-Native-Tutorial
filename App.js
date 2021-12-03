ed90e5475aa62d44ee4e68cf7905400a31cefe6d
Digmoy <digmoy12@gmail.com>
2021-12-03 18:58:39 +0530

    Text & Button

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text,Button,TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+      <Text style={styles.helloTextStyle}> Hello user</Text>
+      <Text style={styles.reactTextStyle}> React Native</Text>
+
+      <Button title="Click Here" color="red" onPress={()=>alert("Welcome to React Native")}disabled={true}></Button>
+      <TouchableOpacity>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>Click Me</Text>
+            </View>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +22,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  helloTextStyle: {
+      fontSize: 20,
+      color: "blue",
+      fontWeight: "bold",
+      fontStyle: "italic"
+  },
+  reactTextStyle: {
+     fontSize: 16,
+     color: "blue",
+     fontWeight: "bold",
+  },
+  button: {
+    borderRadius: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+    backgroundColor: '#f01d71',
+},
+buttonText: {
+    color: 'yellow',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 16,
+    textAlign: 'center',
+}
 });
