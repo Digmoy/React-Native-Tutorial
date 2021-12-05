e117962f0e86f0c56f457260f515dcc4fbb0cb3e
Digmoy <digmoy12@gmail.com>
2021-12-05 21:16:57 +0530

    flexbox

--- App.js
+++ App.js
@@ -1,53 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet,Text,Button,TouchableOpacity, View } from 'react-native';
+import { StyleSheet,Text,Button,TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     
-      <Text style={styles.helloTextStyle}> Hello user</Text>
-      <Text style={styles.reactTextStyle}> React Native</Text>
-
-      <Button title="Click Here" color="red" onPress={()=>alert("Welcome to React Native")}disabled={true}></Button>
-      <TouchableOpacity>
-            <View style={styles.button}>
-                <Text style={styles.buttonText}>Click Me</Text>
-            </View>
-        </TouchableOpacity>
+    <View style={{ padding: 50,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+      {/* <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Enter Name" style={{ width: '80%', borderBottomColor: 'black',borderWidth: 0.5, padding: 10}}/>
+        <Button title="ADD"/>
+      </View> */}
+      <View style={{backgroundColor:'red',flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text style={{color:'white'}}>1</Text>
+      </View>
+      <View style={{backgroundColor:'blue',flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text style={{color:'white'}}>2</Text>
+      </View>
+      <View style={{backgroundColor:'green',flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text style={{color:'white'}}>3</Text>
+      </View>
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  helloTextStyle: {
-      fontSize: 20,
-      color: "blue",
-      fontWeight: "bold",
-      fontStyle: "italic"
-  },
-  reactTextStyle: {
-     fontSize: 16,
-     color: "blue",
-     fontWeight: "bold",
-  },
-  button: {
-    borderRadius: 8,
-    paddingVertical: 14,
-    paddingHorizontal: 10,
-    backgroundColor: '#f01d71',
-},
-buttonText: {
-    color: 'yellow',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    fontSize: 16,
-    textAlign: 'center',
-}
-});
+
+  });
