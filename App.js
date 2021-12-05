0a957b848c2b90be09953974ee19d7f99cc2a83a
Digmoy <digmoy12@gmail.com>
2021-12-05 21:43:16 +0530

    Chnage style

--- App.js
+++ App.js
@@ -1,53 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet,Text,Button,TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, Button, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     
-      <Text style={styles.helloTextStyle}> Hello user</Text>
-      <Text style={styles.reactTextStyle}> React Native</Text>
-
-      <Button title="Click Here" color="red" onPress={()=>alert("Welcome to React Native")}disabled={true}></Button>
-      <TouchableOpacity>
-            <View style={styles.button}>
-                <Text style={styles.buttonText}>Click Me</Text>
-            </View>
-        </TouchableOpacity>
+    <View style={styles.screenStyle}>
+      <View style={styles.inputContainerStyle}>
+        <TextInput placeholder="Enter Name" style={styles.textInputStyle} />
+        <Button title="ADD" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screenStyle: {
+    padding: 50, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
   },
-  helloTextStyle: {
-      fontSize: 20,
-      color: "blue",
-      fontWeight: "bold",
-      fontStyle: "italic"
+  inputContainerStyle: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
   },
-  reactTextStyle: {
-     fontSize: 16,
-     color: "blue",
-     fontWeight: "bold",
-  },
-  button: {
-    borderRadius: 8,
-    paddingVertical: 14,
-    paddingHorizontal: 10,
-    backgroundColor: '#f01d71',
-},
-buttonText: {
-    color: 'yellow',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    fontSize: 16,
-    textAlign: 'center',
-}
+  textInputStyle: { width: '80%', borderBottomColor: 'black', borderWidth: 0.5, padding: 10 }
 });
